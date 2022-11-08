efdb8f3ec6c1b84386c58cf9233d6d739ec70018
abelstor <abelstor@gmail.com>
2022-11-08 06:59:34 -0500

    alert-2

--- src/pages/Matriz/Matriz.tsx
+++ src/pages/Matriz/Matriz.tsx
@@ -4,14 +4,31 @@ import { WebPartContext } from '@microsoft/sp-webpart-base';
 
 import { useList } from '@/hooks';
 import { IEmployees } from '@/interfaces';
+import swal from 'sweetalert';
 
 export interface MatrizInterface {
 	context: WebPartContext;
 }
 
-const Matriz : React.FC<MatrizInterface> = (props) => {
+const Matriz: React.FC<MatrizInterface> = (props) => {
 
 	const [items, loading] = useList('Employees', props.context);
+
+	const confirm = (): void => {
+		swal({
+			title: "Are you sure?",
+			text: "Are you sure that you want to leave this page?",
+			icon: "warning",
+			buttons: ["Cancel", "Yes, I am sure!"],
+			// dangerMode: true,
+		}).then(willDelete => {
+			if (willDelete) {
+				swal("Deleted!", "Your imaginary file has been deleted!", "success").catch(console.log);
+			} else {
+				swal("Your imaginary file is safe!").catch(console.log);
+			}
+		}).catch(console.log);
+	}
 
 	return (
 		<>
@@ -27,10 +44,17 @@ const Matriz : React.FC<MatrizInterface> = (props) => {
 							collapsedIcon={"ChevronDown"}
 							expandedIcon={"ChevronUp"}
 							className={"itemCell"}>
-							<div className={"itemContent"}>
-								<div className={"itemResponse"}>{o.Category}</div>
-								<div className={"itemResponse"}>{o.Company}</div>
-								<div className={"itemIndex"}>{o.LevelOfHappiness}</div>
+							<div className={"itemContent row"}>
+								<div className="col">
+									<div className={"itemResponse"}>{o.Category}</div>
+									<div className={"itemResponse"}>{o.Company}</div>
+									<div className={"itemIndex"}>{o.LevelOfHappiness}</div>
+								</div>
+								<button
+									onClick={confirm}
+									className="btn btn-primary col">
+									confirm
+								</button>
 							</div>
 						</Accordion>
 					))
